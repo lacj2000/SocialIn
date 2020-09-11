e1c569fa6aca69e9a6dae1eb23cb7cdb9792052d
luizjuniordeveloper <luizjuniordeveloper@gmail.com>
2020-09-10 21:55:54 -0300

    atividade 5-criação de models/inserindo elementos

--- socialIn/socialApp/admin.py
+++ socialIn/socialApp/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from socialApp.models import UserN, Profile, Post, Comment,React
 
-# Register your models here.
+admin.site.register(UserN)
+admin.site.register(Profile)
+admin.site.register(Comment)
+admin.site.register(Post)
+admin.site.register(React)
--- socialIn/socialApp/models.py
+++ socialIn/socialApp/models.py
@@ -1,3 +1,54 @@
 from django.db import models
 
-# Create your models here.
+
+class UserN(models.Model):
+    data_de_nascimento = models.DateField()
+    username = models.CharField(max_length=128)
+    email = models.EmailField()
+    password = models.CharField(max_length=128)
+
+    def __str__ (self):
+        return self.username
+
+class Profile(models.Model):
+    name = models.CharField(max_length=128)
+    user = models.ForeignKey(UserN, on_delete=models.CASCADE, related_name = 'profiles')
+    contats = models.ManyToManyField('self', blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+
+
+class Post(models.Model):
+    text = models.TextField()
+    date = models.DateField()
+    creator = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name = 'posts')
+    def __str__(self):
+        return self.text
+
+class Comment(models.Model):
+    text = models.TextField()
+    date = models.DateField()
+    creator = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name = 'comments')
+    post  = models.ForeignKey(Post, on_delete=models.CASCADE, related_name = 'comments')
+    def __str__(self):
+        return ""+self.post.text+": "+self.text
+
+class React(models.Model):
+    REACTS_TYPES = [
+        'Curti',
+        'Amei',
+        'Rindo',
+        'Impressionado',
+        'Entristecido',
+        'Irritado',
+    ]
+    
+    type = models.CharField(max_length=15,choices=REACTS_TYPES)
+    date = models.DateField()
+    post =  models.ForeignKey(Post, on_delete=models.CASCADE, related_name = 'reacts')
+    profile  =  models.ForeignKey(Profile, on_delete=models.CASCADE, related_name = 'reacts')
+    peso = models.IntegerField()
+    def __str__(self):
+        return ""+str(self.peso)+" "+self.type+": "+self.post.text+"by. "+self.profile.name 
